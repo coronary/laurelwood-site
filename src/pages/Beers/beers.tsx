f01c74d36d4c977eecbfae068d8b7d6834245a26
vera <veradekalb@gmail.com>
2026-01-20 19:02:32 -0800

    beer page rough draft

--- src/pages/Beers/beers.tsx
+++ src/pages/Beers/beers.tsx
@@ -1,21 +1,24 @@
+import '~home/App.css'
+import './beers.css'
+import FreeRangeRed from '~assets/sell_sheets/evergreen/free_range_red_sell.png'
+import WorkHorseSmall from '~assets/sell_sheets/evergreen/workhorse_sell_small.png'
+import WorkHorseLarge from '~assets/sell_sheets/evergreen/workhorse_sell_large.png'
 
-import './App.css'
+import CoffeeStout from '~assets/sell_sheets/rotating/cold_brew_sell.png'
+import TreeHugger from '~assets/sell_sheets/rotating/treehugger_sell.png'
+import VinterVarmer from '~assets/sell_sheets/rotating/vinter_varmer_sell.png'
 
 export const Beers = () => {
 	return (
 		<>
-			<h1>Always On</h1>
-			<p>
-				Workhorse 12oz
-				Free Range Red 12oz
-				Workhorse 19.2 oz
-			</p>
-			<h1>Rotating Beers - look for these seasonal brews at your favorite beer retailer now</h1>
-			<p>
-				Coffee Stout
-				Treehugger Porter
-				Vinter Varmer
-			</p>
+			<p className="text-4xl mb-4">Always On</p>
+			<img src={WorkHorseSmall} />
+			<img src={FreeRangeRed} />
+			<img src={WorkHorseLarge} />
+			<p className="text-4xl mb-4">Rotating Beers - look for these seasonal brews at your favorite beer retailer now</p>
+			<img src={CoffeeStout} />
+			<img src={TreeHugger} />
+			<img src={VinterVarmer} />
 		</>
 	)
 }
