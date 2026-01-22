76ddc8842ff22acb80586b8222bc1955229d52d9
vera <veradekalb@gmail.com>
2026-01-21 19:25:34 -0800

    make adding and removing beers easier

---
+++ src/functions/fetchBeers.ts
@@ -0,0 +1,47 @@
+interface Beer {
+	filePath: string;
+	beerName: string;
+	ariaText: string;
+}
+
+const evergreen: Beer[] = [
+	{
+		beerName: "Workhorse IPA 12oz Cans",
+		filePath: "/src/assets/sell_sheets/evergreen/workhorse_sell_small.png",
+		ariaText: "test",
+	},
+	{
+		beerName: "Free Range Red 12oz Cans",
+		filePath: "/src/assets/sell_sheets/evergreen/free_range_red_sell.png",
+		ariaText: "test",
+	},
+	{
+		beerName: "Workhorse IPA 19.2oz Cans",
+		filePath: "/src/assets/sell_sheets/evergreen/workhorse_sell_large.png",
+		ariaText: "test",
+	},
+];
+
+const seasonal: Beer[] = [
+	{
+		beerName: "Cold Brewed Coffee Stout w/ Cocoa Nibs",
+		filePath: "/src/assets/sell_sheets/rotating/cold_brew_sell.png",
+		ariaText: "test",
+	},
+	{
+		beerName: "Tree Hugger Porter",
+		filePath: "/src/assets/sell_sheets/rotating/treehugger_sell.png",
+		ariaText: "test",
+	},
+	{
+		beerName: "Vinter Varmer Winter Ale",
+		filePath: "/src/assets/sell_sheets/rotating/vinter_varmer_sell.png",
+		ariaText: "test",
+	},
+];
+
+const fetchBeers = () => {
+	return { evergreen, seasonal };
+};
+
+export default fetchBeers;
--- src/pages/Beers/beers.tsx
+++ src/pages/Beers/beers.tsx
@@ -1,24 +1,15 @@
 import '~home/App.css'
 import './beers.css'
-import FreeRangeRed from '~assets/sell_sheets/evergreen/free_range_red_sell.png'
-import WorkHorseSmall from '~assets/sell_sheets/evergreen/workhorse_sell_small.png'
-import WorkHorseLarge from '~assets/sell_sheets/evergreen/workhorse_sell_large.png'
-
-import CoffeeStout from '~assets/sell_sheets/rotating/cold_brew_sell.png'
-import TreeHugger from '~assets/sell_sheets/rotating/treehugger_sell.png'
-import VinterVarmer from '~assets/sell_sheets/rotating/vinter_varmer_sell.png'
+import fetchBeers from '../../functions/fetchBeers'
 
 export const Beers = () => {
+	const { evergreen, seasonal } = fetchBeers()
 	return (
 		<>
 			<p className="text-4xl mb-4">Always On</p>
-			<img src={WorkHorseSmall} />
-			<img src={FreeRangeRed} />
-			<img src={WorkHorseLarge} />
+			{evergreen.map(({ beerName, filePath }) => <img key={beerName} src={filePath} />)}
 			<p className="text-4xl mb-4">Rotating Beers - look for these seasonal brews at your favorite beer retailer now</p>
-			<img src={CoffeeStout} />
-			<img src={TreeHugger} />
-			<img src={VinterVarmer} />
+			{seasonal.map(({ beerName, filePath }) => <img key={beerName} src={filePath} />)}
 		</>
 	)
 }
